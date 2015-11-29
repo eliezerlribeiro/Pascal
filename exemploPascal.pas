41d3740846ef2c818020ade514d0e10683a1c5d1
Eliezer <eliezerlrbeiro@yahoo.com.br>
2015-11-29 15:22:40 -0200

    atualizando WHILE

--- exemploPascal.pas
+++ exemploPascal.pas
@@ -3,7 +3,6 @@ var x, y : integer ;
 function mdc (a , b : integer) : integer; 
 begin 
 	begin
-		
 		read(x, y);
 		begin
 			read(x, y);
@@ -11,17 +10,22 @@ begin
 		end
 		
 	end;
+	begin
+		while b = 0  do
+		mdc :=mdc(b, a mod b)
+		
+	end;
 	if b =0 then
 		mdc := a + b
 	else 
 		mdc :=mdc(b, a mod b);
-	  mdc :=mdc(b, a mod b);
+			mdc :=mdc(b, a mod b);
 	   mdc :=mdc(b, a mod b)
 end;
 begin
 	begin
 			read(x, y);
-			write(mdc(x,y))
+		 	write(mdc(x,y))
 		end;
 	read(x, y);
 	write(mdc(x,y))
